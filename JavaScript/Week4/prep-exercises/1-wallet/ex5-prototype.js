86cbfa9ee8ba827b5badc20b21a3dcf4306ca6b7
SFK <salihfurkankara@gmail.com>
2024-12-14 17:33:13 +0100

    js-week4-prep done

---
+++ JavaScript/Week4/prep-exercises/1-wallet/ex5-prototype.js
@@ -0,0 +1,84 @@
+import eurosFormatter from './euroFormatter.js';
+
+function Wallet(name, cash) {
+  this._name = name;
+  this._cash = cash;
+  this._dailyAllowance = 40;
+  this._dayTotalWithdrawals = 0;
+}
+
+Wallet.prototype.deposit = function (amount) {
+  this._cash += amount;
+};
+
+Wallet.prototype.withdraw = function (amount) {
+  if (this._cash - amount < 0) {
+    console.log(`Insufficient funds!`);
+    return 0;
+  }
+
+  if (this._dayTotalWithdrawals + amount > this._dailyAllowance) {
+    console.log("Insufficient remaining daily allowance!")
+    return 0;
+  }
+
+  this._cash -= amount;
+  this._dayTotalWithdrawals += amount;
+  return amount;
+};
+
+Wallet.prototype.transferInto = function (wallet, amount) {
+  console.log(
+    `Transferring ${eurosFormatter.format(amount)} from ${
+      this._name
+    } to ${wallet.getName()}`
+  );
+  const withdrawnAmount = this.withdraw(amount);
+  wallet.deposit(withdrawnAmount);
+};
+
+Wallet.prototype.reportBalance = function () {
+  console.log(
+    `Name: ${this._name}, balance: ${eurosFormatter.format(this._cash)}`
+  );
+};
+
+Wallet.prototype.getName = function () {
+  return this._name;
+};
+
+Wallet.prototype.resetDailyAllowance = function () {
+  this._dayTotalWithdrawals = 0;
+  console.log(
+    `Day total withdrawals reset: ${eurosFormatter.format(0)}`
+  );
+}
+
+Wallet.prototype.setDailyAllowance = function (newAllowance) {
+  this._dailyAllowance = newAllowance;
+  console.log(
+    `Daily allowance set to: ${eurosFormatter.format(newAllowance)}`
+  );
+}
+
+function main() {
+  const walletJack = new Wallet('Jack', 100);
+  const walletJoe = new Wallet('Joe', 10);
+  const walletJane = new Wallet('Jane', 20);
+
+  walletJack.transferInto(walletJoe, 50);
+  walletJack.setDailyAllowance(50);
+  walletJack.transferInto(walletJoe, 50);
+  walletJack.resetDailyAllowance();
+  walletJack.transferInto(walletJoe, 50);
+
+  walletJane.transferInto(walletJoe, 25);
+  walletJane.deposit(20);
+  walletJane.transferInto(walletJoe, 25);
+
+  walletJack.reportBalance();
+  walletJoe.reportBalance();
+  walletJane.reportBalance();
+}
+
+main();
